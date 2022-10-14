888f902f5bdca5b53123a64007fa710db7b0d958
Samuel Souza <samuel.souzagomes001@gmail.com>
2022-10-13 22:56:14 -0300

    feat: connecting components

--- src/components/Checkbox.stories.tsx
+++ src/components/Checkbox.stories.tsx
@@ -9,10 +9,10 @@ export default {
   argTypes: {},
   decorators: [
     (Story) => (
-      <div className='flex items-center gap-2'>
+      <label className='flex items-center gap-2'>
         {Story()}
         <Text>Remember me my self for 30 days.</Text>
-      </div>
+      </label>
     )
   ]
 } as Meta<CheckboxProps>
